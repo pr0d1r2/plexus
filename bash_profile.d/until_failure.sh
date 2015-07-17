2ff80b973e238a70edd32dadd2cbca5e4e10f1a0
Marcin Nowicki <pr0d1r2@gmail.com>
2015-07-17 05:41:46 +0200

    Add run counter

--- bash_profile.d/until_failure.sh
+++ bash_profile.d/until_failure.sh
@@ -1,6 +1,7 @@
 function until_failure() {
   local until_failure_ERR
   local until_failure_SLEEP
+  local until_failure_RUN
   case $SLEEP in
     "")
       until_failure_SLEEP=60
@@ -10,11 +11,14 @@ function until_failure() {
       ;;
   esac
   until_failure_ERR=0
+  until_failure_RUN=1
   while [ $until_failure_ERR -eq 0 ]; do
+    echo "until_failure: run #$until_failure_RUN"
     $@
     until_failure_ERR=$?
     if [ $until_failure_ERR -eq 0 ]; then
       sleep $until_failure_SLEEP
+      until_failure_RUN+=1
     else
       return $until_failure_ERR
     fi
--- bash_profile.d/until_success.sh
+++ bash_profile.d/until_success.sh
@@ -1,6 +1,7 @@
 function until_success() {
   local until_success_ERR
   local until_success_SLEEP
+  local until_success_RUN
   case $SLEEP in
     "")
       until_success_SLEEP=60
@@ -10,11 +11,14 @@ function until_success() {
       ;;
   esac
   until_success_ERR=1
+  until_success_RUN=1
   while [ $until_success_ERR -gt 0 ]; do
+    echo "until_failure: run #$until_success_RUN"
     $@
     until_success_ERR=$?
     if [ $until_success_ERR -gt 0 ]; then
       sleep $until_success_SLEEP
+      until_success_RUN+=1
     fi
   done
 }
