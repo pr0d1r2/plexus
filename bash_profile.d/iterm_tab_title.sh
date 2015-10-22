242356f4b932da74fabea2c3be4c94fae992dad8
Marcin Nowicki <pr0d1r2@gmail.com>
2015-10-22 15:12:15 +0200

    Allow quickly to label iTerm tab

---
+++ bash_profile.d/iterm_tab_title.sh
@@ -0,0 +1,9 @@
+function iterm_tab_title() {
+  if [ "$1" ]
+  then
+    unset PROMPT_COMMAND
+    echo -ne "\033]0;${*}\007"
+  else
+    export PROMPT_COMMAND='echo -ne "\033]0;${PWD/#$HOME/~}\007"'
+  fi
+}
