9ffcdb87e28ea81b99eaecb897973c209c50bc7e
Marcin Nowicki <pr0d1r2@gmail.com>
2015-11-15 05:40:00 +0100

    Adjust script to use current version of mysql

--- bash_profile.d/mysql_reload.sh
+++ bash_profile.d/mysql_reload.sh
@@ -1,4 +1,4 @@
 function mysql_reload() {
-  launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+  launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mysql55.plist
+  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql55.plist
 }
