6533dd72373d47d0a5027f1d351b3e4ea904d4d2
Marcin Nowicki <pr0d1r2@gmail.com>
2016-01-30 04:56:53 +0100

    Adjust alias to be able: merge from exact branch (example: git_merge" master"), merge from updated branch (performs git pull on branch you merge from)

--- bash_profile.d/git_merge.sh
+++ bash_profile.d/git_merge.sh
@@ -1,5 +1,5 @@
 function git_merge() {
-  local git_merge_BRANCH_LIST=`git branch | grep $1`
+  local git_merge_BRANCH_LIST=`git branch | grep "$1"`
   local git_merge_BRANCH_HITS=`echo $git_merge_BRANCH_LIST | wc -l`
   local git_merge_CURRENT_BRANCH=`git status | grep "On branch" | cut -b11-`
   if [ $git_merge_BRANCH_HITS -eq 1 ]; then
@@ -8,6 +8,9 @@ function git_merge() {
       return 1
     fi
     local git_merge_BRANCH_TO_MERGE=`echo $git_merge_BRANCH_LIST | cut -b3-`
+    git checkout $git_merge_BRANCH_TO_MERGE || return $?
+    git pull || return $?
+    git checkout $git_merge_CURRENT_BRANCH || return $?
     git merge $git_merge_BRANCH_TO_MERGE || return $?
   elif [ $git_merge_BRANCH_HITS -eq 0 ]; then
     echo "No branches found when searching for: $1"
