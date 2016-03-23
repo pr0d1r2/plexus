45a639ae461fd9e2748335217fafafd16c4570dd
Marcin Nowicki <pr0d1r2@gmail.com>
2016-03-23 04:05:29 +0100

    Add aliases to perform check on files changed in branch

---
+++ bash_profile.d/git_files_changed_vs_origin_master.sh
@@ -0,0 +1,10 @@
+function git_files_changed_vs_origin_master() {
+  git_current_branch | grep -q "^master$"
+  if [ $? -eq 0 ]; then
+    echo "You are currently in 'master' branch!"
+    return 8472
+  fi
+  git fetch || return $?
+  git diff --name-status origin/master | cut -f 2
+  return $?
+}
---
+++ bash_profile.d/rubocop_changed_in_branch.sh
@@ -0,0 +1,3 @@
+function rubocop_changed_in_branch() {
+  rubocop `git_files_changed_vs_origin_master` || return $?
+}
