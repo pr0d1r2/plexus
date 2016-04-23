225c0bc5ad5e24d2dd6b2746a24277b99bfefd6f
Marcin Nowicki <pr0d1r2@gmail.com>
2016-04-23 13:08:21 +0200

    Adjust script to support deleted files

--- bash_profile.d/bers_changed_vs_origin_master.sh
+++ bash_profile.d/bers_changed_vs_origin_master.sh
@@ -1,7 +1,14 @@
 function bers_changed_vs_origin_master() {
-  local bers_changed_vs_origin_master_FILES=`git_files_changed_vs_origin_master | grep spec | grep "_spec.rb$"`
-  if [ `echo $bers_changed_vs_origin_master_FILES | wc -l` -gt 0 ]; then
-    echorun bers `echo $bers_changed_vs_origin_master_FILES | tr "\n" " "`
+  local bers_changed_vs_origin_master_FILE
+  local bers_changed_vs_origin_master_FILES_EXISTING=()
+  for bers_changed_vs_origin_master_FILE in `git_files_changed_vs_origin_master | grep spec | grep "_spec.rb$"`
+  do
+    if [ -f $bers_changed_vs_origin_master_FILE ]; then
+      bers_changed_vs_origin_master_FILES_EXISTING+=$bers_changed_vs_origin_master_FILE
+    fi
+  done
+  if [ `echo $bers_changed_vs_origin_master_FILES_EXISTING | wc -l` -gt 0 ]; then
+    echorun bers `echo $bers_changed_vs_origin_master_FILES_EXISTING | tr "\n" " "`
   else
     echo
     echo "$0: nothing to run"
