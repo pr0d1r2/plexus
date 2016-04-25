789bcd29709254962ef046333b61723e95f45832
Marcin Nowicki <pr0d1r2@gmail.com>
2016-04-25 10:27:58 +0200

    Skip deleted files

--- bash_profile.d/git_files_changed_vs_origin_master.sh
+++ bash_profile.d/git_files_changed_vs_origin_master.sh
@@ -5,6 +5,6 @@ function git_files_changed_vs_origin_master() {
     return 8472
   fi
   git fetch || return $?
-  git diff --name-status origin/master | cut -f 2
+  git diff --name-status origin/master | grep -v "^D" | cut -f 2
   return $?
 }
