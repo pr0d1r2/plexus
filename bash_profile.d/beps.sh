580e0ca2072f69d521089ed88a73db268cc49cd9
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-16 15:14:18 +0200

    Allow to run specs changed vs master in parallel

--- bash_profile.d/beps.sh
+++ bash_profile.d/beps.sh
@@ -1,7 +1,14 @@
 function beps() {
-  if [ -f knapsack_rspec_report.json ]; then
-    bundle exec `rake_executable` knapsack:rspec
-  else
-    bundle exec parallel_rspec spec
-  fi
+  case $1 in
+    '')
+      if [ -f knapsack_rspec_report.json ]; then
+        bundle exec `rake_executable` knapsack:rspec
+      else
+        bundle exec parallel_rspec spec
+      fi
+      ;;
+    *)
+      bundle exec parallel_rspec $@
+      ;;
+  esac
 }
