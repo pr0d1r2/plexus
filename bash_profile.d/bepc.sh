8840a801199744d9f81564aaaaf5de226469cc7b
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-17 12:46:23 +0200

    Allow specify files to be run in palallel

--- bash_profile.d/bepc.sh
+++ bash_profile.d/bepc.sh
@@ -1,7 +1,14 @@
 function bepc() {
-  if [ -f knapsack_cucumber_report.json ]; then
-    DISABLE_SPRING=1 bundle exec rake knapsack:cucumber
-  else
-    DISABLE_SPRING=1 bundle exec rake parallel:features
-  fi
+  case $1 in
+    '')
+      if [ -f knapsack_cucumber_report.json ]; then
+        DISABLE_SPRING=1 bundle exec rake knapsack:cucumber
+      else
+        DISABLE_SPRING=1 bundle exec parallel_cucumber features
+      fi
+      ;;
+    *)
+      DISABLE_SPRING=1 bundle exec parallel_cucumber $@
+      ;;
+  esac
 }
