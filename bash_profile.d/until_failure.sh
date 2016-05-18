e61839639df7781058f0faf155825e0dfef40f51
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-18 09:23:21 +0200

    Allow to set number of attempts

--- bash_profile.d/until_failure.sh
+++ bash_profile.d/until_failure.sh
@@ -2,6 +2,8 @@ function until_failure() {
   local until_failure_ERR
   local until_failure_SLEEP
   local until_failure_RUN
+  local until_failure_ATTEMPTS
+  local until_failure_ATTEMPT
   case $SLEEP in
     "")
       until_failure_SLEEP=60
@@ -10,8 +12,17 @@ function until_failure() {
       until_failure_SLEEP=$SLEEP
       ;;
   esac
+  case $ATTEMPTS in
+    "")
+      until_failure_ATTEMPTS=10
+      ;;
+    [1-9] | [1-9][0-9] | [1-9][0-9][0-9] | [1-9][0-9][0-9][0-9])
+      until_failure_ATTEMPTS=$ATTEMPTS
+      ;;
+  esac
   until_failure_ERR=0
   until_failure_RUN=1
+  until_failure_ATTEMPT=1
   while [ $until_failure_ERR -eq 0 ]; do
     echo "until_failure: run #$until_failure_RUN (`date`)"
     $@
@@ -23,5 +34,9 @@ function until_failure() {
       echo "until_failure: FAILURE: run #$until_failure_RUN (`date`)"
       return $until_failure_ERR
     fi
+    if [ $until_failure_ATTEMPTS -eq $until_failure_ATTEMPT ]; then
+      return 0
+    fi
+    until_failure_ATTEMPT=$(( $until_failure_ATTEMPT + 1 ))
   done
 }
--- bash_profile.d/until_success.sh
+++ bash_profile.d/until_success.sh
@@ -2,6 +2,7 @@ function until_success() {
   local until_success_ERR
   local until_success_SLEEP
   local until_success_RUN
+  local until_success_ATTEMPT
   case $SLEEP in
     "")
       until_success_SLEEP=60
@@ -10,8 +11,17 @@ function until_success() {
       until_success_SLEEP=$SLEEP
       ;;
   esac
+  case $ATTEMPTS in
+    "")
+      until_success_ATTEMPTS=10
+      ;;
+    [1-9] | [1-9][0-9] | [1-9][0-9][0-9] | [1-9][0-9][0-9][0-9])
+      until_success_ATTEMPTS=$ATTEMPTS
+      ;;
+  esac
   until_success_ERR=1
   until_success_RUN=1
+  until_success_ATTEMPT=1
   while [ $until_success_ERR -gt 0 ]; do
     echo "until_success: run #$until_success_RUN (`date`)"
     $@
@@ -20,6 +30,10 @@ function until_success() {
       sleep $until_success_SLEEP
       until_success_RUN=$(( $until_success_RUN + 1 ))
     fi
+    if [ $until_success_ATTEMPTS -eq $until_success_ATTEMPT ]; then
+      return 1
+    fi
+    until_success_ATTEMPT=$(( $until_success_ATTEMPT + 1 ))
   done
-  echo "until_success: SUCCESS: run #$until_failure_RUN (`date`)"
+  echo "until_success: SUCCESS: run #$until_success_RUN (`date`)"
 }
