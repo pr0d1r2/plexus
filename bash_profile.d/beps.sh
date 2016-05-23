a1c60cbf02b1706d3b5e749590c9fdb57e3b6fa7
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-23 12:02:26 +0200

    Use fail fast to see failures instantly

--- bash_profile.d/beps.sh
+++ bash_profile.d/beps.sh
@@ -2,13 +2,13 @@ function beps() {
   case $1 in
     '')
       if [ -f knapsack_rspec_report.json ]; then
-        DISABLE_SPRING=1 bundle exec rake knapsack:rspec
+        DISABLE_SPRING=1 bundle exec rake "knapsack:rspec[--fail-fast]"
       else
-        DISABLE_SPRING=1 bundle exec parallel_rspec spec
+        DISABLE_SPRING=1 bundle exec parallel_rspec -- --fail-fast -- spec
       fi
       ;;
     *)
-      DISABLE_SPRING=1 bundle exec parallel_rspec $@
+      DISABLE_SPRING=1 bundle exec parallel_rspec -- --fail-fast -- $@
       ;;
   esac
 }
