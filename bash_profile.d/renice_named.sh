5855124ba41326d61d607bedbf9e2d760a2908a3
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-27 12:29:16 +0200

    Prevent noise on terminal

--- bash_profile.d/renice_named.sh
+++ bash_profile.d/renice_named.sh
@@ -2,7 +2,10 @@ function renice_named() {
   local renice_named_NAME
   for renice_named_NAME in $@
   do
-    echo "# renice $renice_loop_LEVEL <$renice_named_NAME>"
-    echorun sudo renice $renice_loop_LEVEL `ps -ax | grep "$renice_named_NAME" | grep -v grep | cut -b1-5`
+    ps -ax | grep "$renice_named_NAME" | grep -v grep | grep -q "$renice_named_NAME"
+    if [ $? -eq 0 ]; then
+      echo "# renice $renice_loop_LEVEL <$renice_named_NAME>"
+      echorun sudo renice $renice_loop_LEVEL `ps -ax | grep "$renice_named_NAME" | grep -v grep | cut -b1-5`
+    fi
   done
 }
