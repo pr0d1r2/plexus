96fcf5a25f5ca8a58493f6a726e05f4b707178e1
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-27 19:02:37 +0200

    Support cucumber tags when splitting features

--- bash_profile.d/split_feature_scenarios.sh
+++ bash_profile.d/split_feature_scenarios.sh
@@ -9,14 +9,14 @@ function split_feature_scenarios() {
   local split_feature_scenarios_SPLITTED_FILE
   for split_feature_scenarios_FEATURE in $@
   do
-    split_feature_scenarios_LINES_REMAINING=`cucumber_scenarios_at_lines $split_feature_scenarios_FEATURE`
+    split_feature_scenarios_LINES_REMAINING=`cucumber_scenarios_or_tags_at_lines $split_feature_scenarios_FEATURE`
     split_feature_scenarios_FIRST_SCENARIO_LINE=`echo $split_feature_scenarios_LINES_REMAINING | head -n 1`
     split_feature_scenarios_CONTENT_BEFORE_FIRST_SCENARIO=`cat $split_feature_scenarios_FEATURE | head -n \`expr $split_feature_scenarios_FIRST_SCENARIO_LINE - 1\``
     split_feature_scenarios_DIRECTORY="${split_feature_scenarios_FEATURE}_splitted"
     if [ ! -d $split_feature_scenarios_DIRECTORY ]; then
       mkdir $split_feature_scenarios_DIRECTORY
     fi
-    for split_feature_scenarios_LINE in `cucumber_scenarios_at_lines $split_feature_scenarios_FEATURE`
+    for split_feature_scenarios_LINE in `cucumber_scenarios_or_tags_at_lines $split_feature_scenarios_FEATURE`
     do
       split_feature_scenarios_LINES_REMAINING=`echo $split_feature_scenarios_LINES_REMAINING | grep -v "^$split_feature_scenarios_LINE$"`
       if [ `echo $split_feature_scenarios_LINES_REMAINING | wc -l` -eq 1 ]; then
