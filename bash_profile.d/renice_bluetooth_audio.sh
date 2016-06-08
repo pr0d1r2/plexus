54a2d5768dda45622e0f756fb6c9397c77b1e4e2
Marcin Nowicki <pr0d1r2@gmail.com>
2016-06-08 18:15:54 +0200

    Do not break in case of error

--- bash_profile.d/renice_bluetooth_audio.sh
+++ bash_profile.d/renice_bluetooth_audio.sh
@@ -1,3 +1,3 @@
 function renice_bluetooth_audio() {
-  sudo renice -18 `ps -ax | grep bluetoothaudiod | grep -v grep | cut -b 1-5` || return $?
+  sudo renice -18 `ps -ax | grep bluetoothaudiod | grep -v grep | cut -b 1-5`
 }
