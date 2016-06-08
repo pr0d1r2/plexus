0ddc89032e8ad25ad1902458b3cb405065fd95e8
Marcin Nowicki <pr0d1r2@gmail.com>
2016-06-08 13:21:24 +0200

    Renice audio with support for USB audio

--- bash_profile.d/renice_audio.sh
+++ bash_profile.d/renice_audio.sh
@@ -1,3 +1,5 @@
 function renice_audio() {
-  sudo renice -18 `ps -ax | grep coreaudiod | grep -v grep | cut -b 1-5` || return $?
+  sudo renice -18 `ps -ax | grep coreaudiod | grep -v grep | cut -b 1-5`
+  sudo renice -18 `ps -ax | grep AudioToolbox.framework | grep -v grep | cut -b 1-5`
+  sudo renice -18 `ps -ax | grep CoreAudio.framework | grep -v grep | cut -b 1-5`
 }
