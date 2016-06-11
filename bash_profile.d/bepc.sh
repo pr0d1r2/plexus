614cfa4eb7a6d3013e570c886877c9a70514d389
Marcin Nowicki <pr0d1r2@gmail.com>
2016-06-11 07:18:33 +0200

    Separate knapsack aliases

--- bash_profile.d/bepc.sh
+++ bash_profile.d/bepc.sh
@@ -1,11 +1,7 @@
 function bepc() {
   case $1 in
     '')
-      if [ -f knapsack_cucumber_report.json ]; then
-        DISABLE_SPRING=1 bundle exec rake knapsack:cucumber
-      else
-        DISABLE_SPRING=1 bundle exec parallel_cucumber features
-      fi
+      DISABLE_SPRING=1 bundle exec parallel_cucumber features
       ;;
     *)
       DISABLE_SPRING=1 bundle exec parallel_cucumber $@
--- bash_profile.d/beps.sh
+++ bash_profile.d/beps.sh
@@ -1,11 +1,7 @@
 function beps() {
   case $1 in
     '')
-      if [ -f knapsack_rspec_report.json ]; then
-        DISABLE_SPRING=1 bundle exec rake "knapsack:rspec[--fail-fast]"
-      else
-        DISABLE_SPRING=1 bundle exec parallel_rspec -- --fail-fast -- spec
-      fi
+      DISABLE_SPRING=1 bundle exec parallel_rspec -- --fail-fast -- spec
       ;;
     *)
       DISABLE_SPRING=1 bundle exec parallel_rspec -- --fail-fast -- $@
