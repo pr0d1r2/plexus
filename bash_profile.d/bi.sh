71f19def93991d34c4bda9fb6d939abe4fae9b4b
Marcin Nowicki <pr0d1r2@gmail.com>
2016-06-11 07:28:09 +0200

    Quadruple bundler threads to speed up installments

--- bash_profile.d/bi.sh
+++ bash_profile.d/bi.sh
@@ -1,7 +1,7 @@
 function bi() {
   is_new_bundler
   if [ $? -eq 0 ]; then
-    bundle install -j`cpu_num` || return $?
+    bundle install -j`bundler_threads` || return $?
   else
     bundle install || return $?
   fi
--- bash_profile.d/bu.sh
+++ bash_profile.d/bu.sh
@@ -1,7 +1,7 @@
 function bu() {
   is_new_bundler
   if [ $? -eq 0 ]; then
-    bundle update -j`cpu_num` $@ || return $?
+    bundle update -j`bundler_threads` $@ || return $?
   else
     bundle update $@ || return $?
   fi
