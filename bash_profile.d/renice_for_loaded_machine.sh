308201dc6ad9b171c3f7394c30d5a8f034343552
Marcin Nowicki <pr0d1r2@gmail.com>
2016-07-12 16:31:15 +0200

    Improve audio renice scripts

--- bash_profile.d/renice_for_loaded_machine.sh
+++ bash_profile.d/renice_for_loaded_machine.sh
@@ -2,6 +2,7 @@ function renice_for_loaded_machine() {
   echorun renice_audio && \
   echorun renice_bluetooth_music && \
   echorun renice_usb && \
+  echorun renice_usb_audio && \
   echorun renice_display_link
   return $?
 }
---
+++ bash_profile.d/renice_usb_audio.sh
@@ -0,0 +1,3 @@
+function renice_usb_audio() {
+  renice_named TruStudioPCUSBAudioPlugin.kext
+}
