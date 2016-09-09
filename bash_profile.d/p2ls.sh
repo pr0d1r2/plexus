cf7125a59f382907de8fb630c235d1af29218db1
Marcin Nowicki <pr0d1r2@gmail.com>
2016-09-09 12:17:21 +0200

    Do not fork if no free thread

--- bash_profile.d/p2ls.sh
+++ bash_profile.d/p2ls.sh
@@ -6,6 +6,7 @@ function p2ls() {
   for p2ls_RSPEC_EXAMPLE in `rspec_list_all_examples $@`
   do
     echo "$p2ls_RSPEC_EXAMPLE"
+    p2ls_first_free_thread
     p2ls_job spring rspec $p2ls_RSPEC_EXAMPLE &
     sleep 0.01
   done
