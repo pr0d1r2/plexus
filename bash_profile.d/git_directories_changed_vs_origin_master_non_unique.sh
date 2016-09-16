158e8818b0bbf2396870bbc8f26854e9c74b8a61
Marcin Nowicki <pr0d1r2@gmail.com>
2016-09-16 07:58:44 +0200

    Use only existing directories

--- bash_profile.d/git_directories_changed_vs_origin_master_non_unique.sh
+++ bash_profile.d/git_directories_changed_vs_origin_master_non_unique.sh
@@ -1,7 +1,11 @@
 function git_directories_changed_vs_origin_master_non_unique() {
   local git_directories_changed_vs_origin_master_non_unique_FILE
+  local git_directories_changed_vs_origin_master_non_unique_DIR
   for git_directories_changed_vs_origin_master_non_unique_FILE in `git_files_changed_vs_origin_master 2>/dev/null`
   do
-    dirname $git_directories_changed_vs_origin_master_non_unique_FILE
+    git_directories_changed_vs_origin_master_non_unique_DIR=`dirname $git_directories_changed_vs_origin_master_non_unique_FILE`
+    if [ -d $git_directories_changed_vs_origin_master_non_unique_DIR ]; then
+      echo $git_directories_changed_vs_origin_master_non_unique_DIR
+    fi
   done
 }
