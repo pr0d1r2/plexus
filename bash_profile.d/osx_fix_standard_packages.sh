03b9511e4c1c11632ab186007fff923df9ecb734
Marcin Nowicki <pr0d1r2@gmail.com>
2016-09-17 04:12:50 +0200

    Correct paste-o

--- bash_profile.d/osx_fix_standard_packages.sh
+++ bash_profile.d/osx_fix_standard_packages.sh
@@ -1,3 +1,3 @@
-function bash_profile.d/osx_fix_standard_packages() {
+function osx_fix_standard_packages() {
   sudo /usr/libexec/repair_packages --repair --standard-pkgs --volume / || return $?
 }
