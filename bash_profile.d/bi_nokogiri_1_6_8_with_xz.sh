fde3719518042b60cdc0eeaf9ce144be1355620e
Marcin Nowicki <pr0d1r2@gmail.com>
2016-11-28 06:56:13 +0100

    Make fix work even if something depends on xz

--- bash_profile.d/bi_nokogiri_1_6_8_with_xz.sh
+++ bash_profile.d/bi_nokogiri_1_6_8_with_xz.sh
@@ -2,7 +2,7 @@ function bi_nokogiri_1_6_8_with_xz() {
   which xz 1>/dev/null 2>/dev/null
   local bi_nokogiri_1_6_8_with_xz_NOT_PRESENT=$?
   if [ $bi_nokogiri_1_6_8_with_xz_NOT_PRESENT -eq 0 ]; then
-    brew uninstall xz || return $?
+    brew uninstall --ignore-dependencies xz || return $?
   fi
   bi || return $?
   if [ $bi_nokogiri_1_6_8_with_xz_NOT_PRESENT -eq 0 ]; then
