ff291f86ec4e63cf3b5c562d7ccaad9e95e05167
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-14 10:30:39 +0100

    Enrich checkout script

--- bash_profile.d/git_checkout_matching.sh
+++ bash_profile.d/git_checkout_matching.sh
@@ -2,7 +2,7 @@ function git_checkout_matching() {
   local git_checkout_matching_BRANCH=`git branch | grep "$@"`
   if [ `echo $git_checkout_matching_BRANCH | wc -l` -gt 1 ]; then
     echo
-    echo "No unique branch for '$@':"
+    echo "No unique local branch for '$@':"
     echo $git_checkout_matching_BRANCH
     echo
     return 1
@@ -10,11 +10,13 @@ function git_checkout_matching() {
   case $git_checkout_matching_BRANCH in
     "")
       echo
-      echo "No branch for '$@'"
+      echo "No local branch for '$@'"
       echo
-      return 2
+      echorun git_checkout_matching_remote $@ || return $?
+      ;;
+    *)
+      git_checkout_matching_BRANCH=`echo $git_checkout_matching_BRANCH | cut -b3-`
+      git checkout $git_checkout_matching_BRANCH || return $?
       ;;
   esac
-  git_checkout_matching_BRANCH=`echo $git_checkout_matching_BRANCH | cut -b3-`
-  git checkout $git_checkout_matching_BRANCH || return $?
 }
